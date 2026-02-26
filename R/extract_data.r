f64b7c2b041e5ed353eaedf1f38c6045f12b1a94
andrkoles <andreas.kolesidis@gmail.com>
2026-02-26 13:48:11 +0200

    Created an 'ago' column in quakes table

--- R/extract_data.r
+++ R/extract_data.r
@@ -4,13 +4,13 @@ library(dplyr)
 library(tidyRSS)
 
 # RSS feed URL
-rss <- "http://www.geophysics.geol.uoa.gr/stations/maps/seismicity.xml"
-
+# 
 # Data source:
 # Seismological Laboratory of National and Kapodistrian University
 # of Athens (http://dggsl.geol.uoa.gr/en_index.html)
 
-# Read the RSS feed
+rss <- "http://www.geophysics.geol.uoa.gr/stations/maps/seismicity.xml"
+
 rss_df <- tidyRSS::tidyfeed(rss, parse_dates = FALSE)
 
 # The item_description column holds data for each earthquake in string form
@@ -25,7 +25,8 @@ data <-
     patterns = c(
       place = "[0-9]+\\.[0-9]+\\skm\\s[A-Z]+\\sof\\s[A-z]+",
       "\\s+Time:\\s+",
-      time = "[0-9]+\\-[A-z]{3}-[0-9]+\\s[0-9]+\\:[0-9]+\\:[0-9]+\\s\\(UTC\\)",
+      time = "[0-9]+\\-[A-z]{3}-[0-9]+\\s[0-9]+\\:[0-9]+\\:[0-9]+",
+      "\\s\\(UTC\\)",
       "\\s+Latitude:\\s+",
       latitude = "[0-9]{2}\\.[0-9]{2}",
       "[A-Z]\\s+Longitude:\\s+",
@@ -37,9 +38,19 @@ data <-
     )
   )
 
-# Creates an id column, which facilitates the row to map interaction feature
+date_format <- "%e-%b-%Y %H:%M:%S"
+
+data <- 
+  data |> 
+  mutate(
+    time = paste0(time, " UTC"),
+    ago = difftime(Sys.time(), 
+                   as.POSIXct(time, format = date_format, tz = "UTC"),
+                   units = "hours")
+  )
+
+# The id will facilitate the table row to map interaction 
 data$id <- 1:nrow(data)
 
-# Converts the following columns to numeric type 
-columns <- c("latitude", "longitude", "depth", "magnitude")
+columns <- c("latitude", "longitude", "depth", "magnitude", "ago")
 data[columns] <- map(data[columns], as.numeric)
