89828bf37a904a37b8f718aea20a63d809db5e91
andrkoles <andreas.kolesidis@gmail.com>
2026-03-14 01:19:46 +0200

    Refactored extract_data function and dataServer module

--- R/dataServer.R
+++ R/dataServer.R
@@ -2,12 +2,21 @@ library(shiny)
 
 source("R/extract_data.r")
 
+# RSS feed URL
+# 
+# Data source:
+# Seismological Laboratory of National and Kapodistrian University
+# of Athens (http://dggsl.geol.uoa.gr/en_index.html)
+
+url <- "http://www.geophysics.geol.uoa.gr/stations/maps/seismicity.xml"
+
 dataServer <- function(input, output, session) {
   return(list(
     quakes = reactive({
       # 1 sec = 1000 ms
       invalidateLater(2 * 1000 * 60)
-      data <- extract_data()
+      data <- read_rss(url) |> 
+        extract_data()
       return(data)
     })
   ))
--- R/extract_data.r
+++ R/extract_data.r
@@ -3,57 +3,74 @@ library(purrr)
 library(dplyr)
 library(tidyRSS)
 
-extract_data <- function() {
+# RSS feed URL
+# 
+# Data source:
+# Seismological Laboratory of National and Kapodistrian University
+# of Athens (http://dggsl.geol.uoa.gr/en_index.html)
+
+read_rss <- function(url) {
+  tryCatch({
+    tidyRSS::tidyfeed(url, parse_dates = FALSE) |> 
+      select(item_description)
+  },
+  error = function(e) {
+    message("Could not read RSS feed, make sure the URL is valid.")
+  })
+}
+
+extract_data <- function(rss_df) {
   
-  # RSS feed URL
-  # 
-  # Data source:
-  # Seismological Laboratory of National and Kapodistrian University
-  # of Athens (http://dggsl.geol.uoa.gr/en_index.html)
-  rss <- "http://www.geophysics.geol.uoa.gr/stations/maps/seismicity.xml"
+  if (is.null(rss_df)) {
+    message("No data to extract from.")
+    return(NULL)
+  }
   
-  rss_df <- tidyRSS::tidyfeed(rss, parse_dates = FALSE)
-  
-  # The item_description column holds data for each earthquake in string form
-  description <- rss_df |>
-    select(item_description)
-  
-  # Using regular expressions, extracts six variables for each earthquake
-  data <-
-    description |>
-    tidyr::separate_wider_regex(
-      item_description,
-      patterns = c(
-        place = "[0-9]+\\.[0-9]+\\skm\\s[A-Z]+\\sof\\s[A-z]+",
-        "\\s+Time:\\s+",
-        time = "[0-9]+\\-[A-z]{3}-[0-9]+\\s[0-9]+\\:[0-9]+\\:[0-9]+",
-        "\\s\\(UTC\\)",
-        "\\s+Latitude:\\s+",
-        latitude = "[0-9]{2}\\.[0-9]{2}",
-        "[A-Z]\\s+Longitude:\\s+",
-        longitude = "[0-9]{2}\\.[0-9]{2}",
-        "[A-Z]\\s+Depth:\\s+",
-        depth = "[0-9]+",
-        "km\\s+M\\s+",
-        magnitude = "[0-9]+\\.[0-9]+"
+  tryCatch({
+    # The item_description column holds data for each earthquake in string format
+    description <- rss_df |>
+      select(item_description)
+    
+    # Using regular expressions, extracts six variables for each earthquake
+    data <-
+      description |>
+      tidyr::separate_wider_regex(
+        item_description,
+        patterns = c(
+          place = "[0-9]+\\.[0-9]+\\skm\\s[A-Z]+\\sof\\s[A-z]+",
+          "\\s+Time:\\s+",
+          time = "[0-9]+\\-[A-z]{3}-[0-9]+\\s[0-9]+\\:[0-9]+\\:[0-9]+",
+          "\\s\\(UTC\\)",
+          "\\s+Latitude:\\s+",
+          latitude = "[0-9]{2}\\.[0-9]{2}",
+          "[A-Z]\\s+Longitude:\\s+",
+          longitude = "[0-9]{2}\\.[0-9]{2}",
+          "[A-Z]\\s+Depth:\\s+",
+          depth = "[0-9]+",
+          "km\\s+M\\s+",
+          magnitude = "[0-9]+\\.[0-9]+"
+        )
       )
-    )
-  
-  date_format <- "%e-%b-%Y %H:%M:%S"
-  
-  data <- 
-    data |> 
-    mutate(
-      time = paste0(time, " UTC"),
-      ago = difftime(Sys.time(), 
-                     as.POSIXct(time, format = date_format, tz = "UTC"),
-                     units = "hours")
-    )
-  
-  # The id column is necessary for the table row to map interaction
-  data$id <- 1:nrow(data)
-  
-  columns <- c("latitude", "longitude", "depth", "magnitude", "ago")
-  data[columns] <- map(data[columns], as.numeric)
-  return(data)
+    
+    date_format <- "%e-%b-%Y %H:%M:%S"
+    
+    data <- 
+      data |> 
+      mutate(
+        time = paste0(time, " UTC"),
+        ago = difftime(Sys.time(), 
+                       as.POSIXct(time, format = date_format, tz = "UTC"),
+                       units = "hours")
+      )
+    
+    # The id column is necessary for the table row to map interaction
+    data$id <- 1:nrow(data)
+    
+    columns <- c("latitude", "longitude", "depth", "magnitude", "ago")
+    data[columns] <- map(data[columns], as.numeric)
+    return(data)
+  },
+  error = function(e) {
+    message("Could not extract earthquake data successfully.")
+  })
 }
